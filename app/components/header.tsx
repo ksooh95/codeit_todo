a9d40d7c33edf9784b6837dc9a878e3e1d1f1a43
KIMSOOHWAN <ksooh95@gmail.com>
2024-06-25 19:39:55 +0900

    Feat : codeit todo 과제 초기세팅

---
+++ app/components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <div className="header">
+            <div className="container">
+                <div className="logo">
+                    <img src="/logo_large.png" alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ app/components/search.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+    return (
+        <div className="search">
+            <div className="serach_bar">
+                <img src="search.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Search;
